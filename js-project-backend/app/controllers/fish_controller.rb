6b4b98f139aa17c11e330eee4d72202110ed2579
palepanorama <matthew.keith.thomas@gmail.com>
2021-01-05 14:11:27 -0600

    adds index and show actions to fish controller

--- js-project-backend/app/controllers/fish_controller.rb
+++ js-project-backend/app/controllers/fish_controller.rb
@@ -1,2 +1,11 @@
 class FishController < ApplicationController
+    def index 
+        fish = Fish.all 
+        render json: fish 
+    end 
+
+    def show 
+        fish = Fish.find_by(id: params[:id])
+        render json: fish 
+    end 
 end
