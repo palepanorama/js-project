3f48abbcdc52eb3f9dbdacdc4a94422268e92aa4
palepanorama <matthew.keith.thomas@gmail.com>
2021-01-12 07:24:47 -0600

    adds create method to fish controller

--- backend/app/controllers/fish_controller.rb
+++ backend/app/controllers/fish_controller.rb
@@ -4,6 +4,16 @@ class FishController < ApplicationController
         render json: fish 
     end 
 
+    def create
+        @fish = Fish.create_or_find_by(fish_params)
+
+        if @fish.save
+        render json: @fish, status: :created, location: @fish
+        else
+        render json: @fish.errors, status: :unprocessable_entity
+        end
+    end
+
     def show 
         fish = Fish.find_by(id: params[:id])
         render json: fish 
