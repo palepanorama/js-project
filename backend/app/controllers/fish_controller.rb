6af76be3b3b927ccf239b08a0b796fd661da68a4
palepanorama <matthew.keith.thomas@gmail.com>
2021-01-19 09:06:53 -0600

    bug fixes

--- backend/app/controllers/fish_controller.rb
+++ backend/app/controllers/fish_controller.rb
@@ -1,4 +1,6 @@
 class FishController < ApplicationController
+
+
     def index 
         @fish = Fish.all
         render json: @fish 
@@ -8,23 +10,26 @@ class FishController < ApplicationController
         @fish = Fish.create_or_find_by(fish_params)
 
         if @fish.save
-        render json: @fish, status: :created, location: @fish
+            render json: @fish, status: :created, location: @fish
         else
-        render json: @fish.errors, status: :unprocessable_entity
+            render json: @fish.errors, status: :unprocessable_entity
         end
     end
 
     def show 
-        fish = Fish.find_by(id: params[:id])
-        render json: fish 
+        @fish = Fish.find_by(id: params[:id])
+        render json: @fish 
     end 
 
-    def destroy 
-        @fish.destroy 
-    end 
+    def destroy
+       @fish = Fish.find_by(id: params[:id])
+       @fish.destroy 
+    end
+    
     
     private 
     def fish_params
-        params.require(:fish).permit(:name, :buyer_id, :price)
+        params.require(:fish).permit(:name, :id, :buyer_id, :price)
     end 
+
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :fish
+  resources :fish 
   resources :buyers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
